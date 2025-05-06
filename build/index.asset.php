f30d37881b107376b8c4628ca43cd3f997ea69ca
Matthias Ulrich <post@ulrich.digital>
2025-05-06 10:42:17 +0200

    Update index.asset.php

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-plugins'), 'version' => '301cccb1b70d8f4ff6c1');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-plugins'), 'version' => '5636df93938ea8ea6fe7');
